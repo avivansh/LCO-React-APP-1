fec9b76ddb0c4a2f10e0ff4def8c00da37e943da
Vansh Mittal <vanshmittal1299@gmail.com>
2022-12-24 20:37:54 +0530

    added statecounter code

--- src/App.js
+++ src/App.js
@@ -1,4 +1,4 @@
-import React from "react";
+import React, { useState } from "react";
 
 import Navbar from "./Navbar";
 import Footer from "./Footer";
@@ -6,15 +6,34 @@ import AppSection from "./AppSection";
 import CardSection from "./CardSection";
 import HeroSection from "./HeroSection";
 
+import "./StateCounter.css";
+
 const App = () => {
+  const [count, setCount] = useState(0);
   return (
     <div>
-      <h1> vansh </h1>
+      <h1> Section 1 </h1>
       <Navbar />
       <HeroSection />
       <AppSection />
       <CardSection />
       <Footer />
+      <hr />
+      <h1> Section 2 </h1>
+      <div className="App">
+        <header>
+          <h2> counter app using state/hooks</h2>
+        </header>
+        <h2> Current value of the count is:- {count}</h2>
+        <button onClick={() => setCount(0)}> Reset Couň̌ter</button>
+        <button onClick={() => (count > 10 ? "" : setCount(count + 1))}>
+          Increase Couň̌ter
+        </button>
+        <button onClick={() => (count === 0 ? "" : setCount(count - 1))}>
+          {" "}
+          Decrease Couň̌ter
+        </button>
+      </div>
     </div>
   );
 };
