8927149bfad2312b88c8d5ef4ba4c57bc02127db
Vansh Mittal <vanshmittal1299@gmail.com>
2022-12-24 21:36:18 +0530

    added packages and imported required icons for tic-tac-toe

--- src/App.js
+++ src/App.js
@@ -1,15 +1,37 @@
 import React, { useState } from "react";
+import { ToastContainer, toast } from "react-toastify";
+import "react-toastify/dist/ReactToastify.css";
 
 import Navbar from "./Navbar";
 import Footer from "./Footer";
 import AppSection from "./AppSection";
 import CardSection from "./CardSection";
 import HeroSection from "./HeroSection";
+import Icons from "./components/Icons";
 
-import "./StateCounter.css";
+import { Button, Card, CardBody, Container, Col, Row } from "reactstrap";
+import "bootstrap/dist/css/bootstrap.css";
+import "./App.scss";
+
+const itemArray = new Array(9).fill("empty");
 
 const App = () => {
   const [count, setCount] = useState(0);
+  const [isCross, setIsCross] = useState(0);
+  const [winMessage, setWinMessage] = useState("");
+
+  const reloadGame = () => {
+    //
+  };
+
+  const checkIsWinner = () => {
+    //
+  };
+
+  const changeItem = (itemNumber) => {
+    //
+  };
+
   return (
     <div>
       <h1> Section 1 </h1>
@@ -20,7 +42,7 @@ const App = () => {
       <Footer />
       <hr />
       <h1> Section 2 </h1>
-      <div className="App">
+      <div className="StateCounter">
         <header>
           <h2> counter app using state/hooks</h2>
         </header>
@@ -30,9 +52,13 @@ const App = () => {
           Increase Couň̌ter
         </button>
         <button onClick={() => (count === 0 ? "" : setCount(count - 1))}>
-          {" "}
           Decrease Couň̌ter
         </button>
+      </div>
+      <hr />
+      <h1> Section 3 </h1>
+      <div className="app__tic-tac-toe">
+        <Icons />
       </div>
     </div>
   );
