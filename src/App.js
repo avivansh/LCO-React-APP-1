5472b3ae7570d53a095e083d91ea9dc6a34c6b7f
Vansh Mittal <vanshmittal1299@gmail.com>
2022-12-24 19:54:55 +0530

    added the remaining section of a webpage

--- src/App.js
+++ src/App.js
@@ -1,12 +1,20 @@
 import React from "react";
 
 import Navbar from "./Navbar";
+import Footer from "./Footer";
+import AppSection from "./AppSection";
+import CardSection from "./CardSection";
+import HeroSection from "./HeroSection";
 
 const App = () => {
   return (
     <div>
       <h1> vansh </h1>
       <Navbar />
+      <HeroSection />
+      <AppSection />
+      <CardSection />
+      <Footer />
     </div>
   );
 };
